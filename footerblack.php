f3bf8f20f75253daa31b1e68e9a184ef6e1f0b92
Yoann Abran <y.abran@codeur.online>
2020-07-06 16:59:15 +0200

    credit BGwhite & black

--- footerblack.php
+++ footerblack.php
@@ -1,4 +1,5 @@
 <div class="text-light" id="footerblack">
 <h3>contact</h3>
 <p>adresse mail : </p>
+<p>crédits backgroud carbone : <a href="http://www.freepik.com">Designed by starline / Freepik</a></p>
 </div>
--- footerwhite.php
+++ footerwhite.php
@@ -1,4 +1,5 @@
 <div class="text-dark" id="footerwhite">
 <h3>contact</h3>
 <p>adresse mail : </p>
+<p>crédits backgrounds blanc : <a href="http://www.freepik.com">Designed by Freepik</a></p>
 </div>
